8317df80352eb96c74fb85512995cdb41bf5bd10
Julio Matias <juliocesark2@hotmail.com>
2025-02-14 00:12:36 -0300

    feat: implement Header in Layout by aplication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+import '@/styles/globals.css';
+
+import { Header } from '@/components/Header';
+import { Layout } from '@/components/Layout';
 
 export const metadata: Metadata = {
   title: 'Radio Browser',
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className="bg-secondary text-white">
+        <Header />
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
