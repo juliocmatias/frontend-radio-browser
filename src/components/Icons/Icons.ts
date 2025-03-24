3113ced67bf34a4bd0682d358c541b1d3e8fe011
Julio Matias <juliocesark2@hotmail.com>
2025-03-24 20:48:12 -0300

    feat: add Stop, Play, Edit, and Delete icons to Icons component

--- src/components/Icons/Icons.ts
+++ src/components/Icons/Icons.ts
@@ -3,5 +3,9 @@
 export {
   X as CloseIcon,
   List as OpenIcon,
-  MagnifyingGlass as SearchIcon
+  MagnifyingGlass as SearchIcon,
+  Stop as StopIcon,
+  Play as PlayIcon,
+  PencilSimple as EditIcon,
+  Trash as DeleteIcon
 } from '@phosphor-icons/react';
